415204b7c25e23db74ea08b70f7a02079842edc2
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2020-10-24 13:21:28 +0100

    Passing test for FizzBuzz, Fizz and Buzz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -5,7 +5,11 @@ export const fizzbuzz = (number: number): string[] => {
 
     let array = [];
     for (let i = 1; i <= number; i++){
-        if (i % 3 === 0){
+        if (i % 3 === 0 && i % 5 === 0){
+            array.push("FizzBuzz");
+        }else if (i % 5 == 0) {
+            array.push("Buzz");
+        }else if (i % 3 === 0){
             array.push("Fizz");
         }else {
             array.push(i.toString());
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -7,7 +7,8 @@ describe("Fizzbuzz", () => {
             [1, ["1"]],
             [2, ["1", "2"]],
             [3, ["1", "2", "Fizz"]],
-            [4, ["1", "2", "Fizz", "4"]]
+            [4, ["1", "2", "Fizz", "4"]],
+            [15, ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]]
     ]).test(
         "returns %s for %s", (input, expected) => {
             expect(fizzbuzz(input)).toEqual(expected);
