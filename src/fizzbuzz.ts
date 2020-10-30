03e69292f7e181cc9f707a7734b4d329170259b0
Tom <thomas.spencer@codurance.com>
2020-10-30 07:26:46 +0000

    Add linting

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,26 +1,26 @@
 export const fizzbuzz = (number: number): string[] => {
-    if (number === 1){
-        return [number.toString()];
-    }
+  if (number === 1) {
+    return [number.toString()];
+  }
 
-    function isDivisibleByThreeAndFive(): boolean {
-        return number % 3 === 0 && number % 5 === 0;
-    }
-    function isDivisibleBy(input: number): boolean {
-        return number % input == 0;
-    }
+  function isDivisibleByThreeAndFive(): boolean {
+    return number % 3 === 0 && number % 5 === 0;
+  }
+  function isDivisibleBy(input: number): boolean {
+    return number % input == 0;
+  }
 
-    let array = fizzbuzz(number - 1);
+  const array = fizzbuzz(number - 1);
 
-    if (isDivisibleByThreeAndFive()){
-            array.push("FizzBuzz");
-        }else if (isDivisibleBy(5)) {
-            array.push("Buzz");
-        }else if (isDivisibleBy(3)){
-            array.push("Fizz");
-        }else {
-            array.push(number.toString());
-    }
+  if (isDivisibleByThreeAndFive()) {
+    array.push("FizzBuzz");
+  } else if (isDivisibleBy(5)) {
+    array.push("Buzz");
+  } else if (isDivisibleBy(3)) {
+    array.push("Fizz");
+  } else {
+    array.push(number.toString());
+  }
 
-    return array;
-}
+  return array;
+};
