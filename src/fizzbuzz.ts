618480bd35585c81d84485a53f16acc024f8970e
Tom <thomas.spencer@codurance.com>
2020-10-30 07:42:13 +0000

    Change recursive function to map.

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -3,24 +3,26 @@ export const fizzbuzz = (number: number): string[] => {
     return [number.toString()];
   }
 
-  function isDivisibleByThreeAndFive(): boolean {
-    return number % 3 === 0 && number % 5 === 0;
+  function isDivisibleByThreeAndFive(count: number): boolean {
+    return count % 3 === 0 && count % 5 === 0;
   }
-  function isDivisibleBy(input: number): boolean {
-    return number % input == 0;
-  }
-
-  const array = fizzbuzz(number - 1);
-
-  if (isDivisibleByThreeAndFive()) {
-    array.push("FizzBuzz");
-  } else if (isDivisibleBy(5)) {
-    array.push("Buzz");
-  } else if (isDivisibleBy(3)) {
-    array.push("Fizz");
-  } else {
-    array.push(number.toString());
+  function isDivisibleBy(input: number, count: number): boolean {
+    return count % input == 0;
   }
 
-  return array;
+  const array = Array.from(Array(number + 1).keys());
+
+  const result = array.filter(Number).map((n) => {
+    if (isDivisibleByThreeAndFive(n)) {
+      return "FizzBuzz";
+    } else if (isDivisibleBy(5, n)) {
+      return "Buzz";
+    } else if (isDivisibleBy(3, n)) {
+      return "Fizz";
+    } else {
+      return n.toString();
+    }
+  });
+
+  return result;
 };
